1e7d87f6b430457d8978c1d8def1ec9630ddef01
Faouziali <aly_faouzi@hotmail.com>
2024-02-09 15:37:27 -0500

    Modified for troubleshooting

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -26,6 +26,7 @@ def load_raw_table(session, tname=None, s3dir=None, year=None, schema=None):
     session.use_schema(schema)
     if year is None:
         location = "@external.frostbyte_raw_stage/{}/{}".format(s3dir, tname)
+        #print(location)
     else:
         print('\tLoading year {}'.format(year)) 
         location = "@external.frostbyte_raw_stage/{}/{}/year={}".format(s3dir, tname, year)
